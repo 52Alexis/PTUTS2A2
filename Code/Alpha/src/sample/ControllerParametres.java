da35096b33006c679269e679942d9d40a57e1d15
Moa <lucas.heidet@orange.fr>
2021-06-07 13:51:42 +0200

    Test modif param

--- Code/Alpha/src/sample/ControllerParametres.java
+++ Code/Alpha/src/sample/ControllerParametres.java
@@ -1,4 +1,96 @@
 package sample;
 
-public class ControllerParametres {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.control.Alert;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class ControllerParametres implements EventHandler<ActionEvent> {
+    protected ModelParametres modelParametres;
+    protected ViewParametres viewParametres;
+
+    public ControllerParametres(ModelParametres modelParametres, ViewParametres viewParametres) {
+        this.modelParametres = modelParametres;
+        this.viewParametres = viewParametres;
+        viewParametres.setController(this);
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if (actionEvent.getSource().equals(viewParametres.fieldUp)){
+            viewParametres.scene.setOnKeyPressed(keyEvent -> {
+                String tmp;
+                if (keyEvent.getCode().equals(KeyCode.UP)){
+                    tmp = "UP";
+                }else if (keyEvent.getCode().equals(KeyCode.DOWN)){
+                    tmp = "DOWN";
+                }else if (keyEvent.getCode().equals(KeyCode.RIGHT)){
+                    tmp = "RIGHT";
+                }else if (keyEvent.getCode().equals(KeyCode.LEFT)){
+                    tmp = "LEFT";
+                }else{
+                    tmp = keyEvent.getCode().toString();
+                }
+                if(stringChecker(tmp)){
+                    viewParametres.fieldUp.setText(tmp);
+                    modelParametres.getConfigTouches().set(0,tmp);
+                    modelParametres.updateConfig();
+                    viewParametres.scene.setOnKeyPressed(null);
+                }
+            });
+        }
+
+        if (actionEvent.getSource().equals(viewParametres.fieldDown)){
+            viewParametres.scene.setOnKeyPressed(keyEvent -> {
+                String tmp = keyEvent.getCode().getName();
+                if(stringChecker(tmp)){
+                    viewParametres.fieldDown.setText(tmp);
+                    modelParametres.getConfigTouches().set(1,tmp);
+                }
+            });
+        }
+
+        if (actionEvent.getSource().equals(viewParametres.fieldRight)){
+            viewParametres.scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent keyEvent) {
+                    String tmp = keyEvent.getCode().getName();
+                    if(stringChecker(tmp)){
+                        viewParametres.fieldRight.setText(tmp);
+                        modelParametres.getConfigTouches().set(2,tmp);
+                    }
+                }
+            });
+
+        }
+
+        if (actionEvent.getSource().equals(viewParametres.fieldLeft)){
+            viewParametres.scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent keyEvent) {
+                    String tmp = keyEvent.getCode().toString();
+                    if(stringChecker(tmp)){
+                        viewParametres.fieldLeft.setText(tmp);
+                        modelParametres.getConfigTouches().set(3,tmp);
+                    }
+                }
+            });
+
+        }
+    }
+
+    public boolean stringChecker(String s){
+        if (s.matches(viewParametres.fieldDown.getText()) || s.matches(viewParametres.fieldRight.getText()) ||
+            s.matches(viewParametres.fieldLeft.getText())){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Key already assigned");
+            alert.setContentText("You have already assigned this key to another function.");
+            alert.initOwner(viewParametres.primaryStage);
+            alert.showAndWait();
+            return false;
+        }
+        return true;
+    }
 }
--- Code/Alpha/src/sample/ViewParametres.java
+++ Code/Alpha/src/sample/ViewParametres.java
@@ -1,15 +1,51 @@
 package sample;
 
-import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ViewParametres {
     protected ModelParametres modelParametres;
     protected Stage primaryStage;
-    protected TextField fieldUp;
-    protected TextField fieldDown;
-    protected TextField fieldRight;
-    protected TextField fieldLeft;
+    protected Button fieldUp;
+    protected Button fieldDown;
+    protected Button fieldRight;
+    protected Button fieldLeft;
+    protected Scene scene;
 
+    public ViewParametres(ModelParametres modelParametres, Stage primaryStage) {
+        this.modelParametres = modelParametres;
+        this.primaryStage = primaryStage;
+        init();
+        addWidgetsToView();
+    }
 
+    public void init(){
+        fieldUp = new Button();
+        fieldUp.setText(modelParametres.getConfigTouches().get(0));
+        fieldDown = new Button();
+        fieldDown.setText(modelParametres.getConfigTouches().get(1));
+        fieldRight = new Button();
+        fieldRight.setText(modelParametres.getConfigTouches().get(2));
+        fieldLeft = new Button();
+        fieldLeft.setText(modelParametres.getConfigTouches().get(3));
+    }
+
+    public void addWidgetsToView(){
+        Pane root = new VBox();
+        root.getChildren().addAll(fieldUp,fieldDown,fieldRight,fieldLeft);
+        scene = new Scene(root,200,200);
+        primaryStage.setScene(scene);
+    }
+
+    public void setController(EventHandler< ActionEvent > handler){
+        fieldUp.setOnAction(handler);
+        fieldDown.setOnAction(handler);
+        fieldRight.setOnAction(handler);
+        fieldLeft.setOnAction(handler);
+    }
 }
