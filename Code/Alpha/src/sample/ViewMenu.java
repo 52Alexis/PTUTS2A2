8a9e6d68ae884ae51ff9f48deb5fa9095e426076
Moa <lucas.heidet@orange.fr>
2021-06-08 09:23:17 +0200

    Creation scores

--- Code/Alpha/src/sample/ViewMenu.java
+++ Code/Alpha/src/sample/ViewMenu.java
@@ -1,17 +1,11 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
